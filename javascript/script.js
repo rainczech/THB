b42c00131adc4d47ad3d9f92fff595407db4dc50
Grace Ho <rainczech@gmail.com>
2018-06-04 18:56:19 -0500

    carousel to mostly work

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    // var instance = M.Carousel.getInstance(elem);
-    var instance = M.Carousel.init({
+$(document).ready(function(){
+    $('.carousel').carousel({
         fullWidth: true,
         indicators: true,
         duration: 300
-      });
+    });
 });
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.carousel');
+//     // var instance = M.Carousel.getInstance(elem);
+//     var instance = M.Carousel.init({
+//         fullWidth: true,
+//         indicators: true,
+//         duration: 300
+//       });
+// });
