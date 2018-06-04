9008378fb33117c89a5cf831ebbaf4c40537045a
Grace Ho <rainczech@gmail.com>
2018-06-03 19:43:47 -0500

    fixed JS errors

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    // var instance = M.Carousel.getInstance(elem);
+    var instance = M.Carousel.init({
+        fullWidth: true,
+        indicators: true
+      });
+});
