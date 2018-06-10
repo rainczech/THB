e92e952ad9ea3d837a7beaa70a86fe03c8477ae8
Grace Ho <rainczech@gmail.com>
2018-06-10 10:18:26 -0500

    removed carousel, added parallax

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,18 @@
+// $(document).ready(function(){
+//     $('.carousel').carousel({
+//         fullWidth: true,
+//         indicators: true,
+//         duration: 500
+//     });
+//     autoplay()
+//     function autoplay(){
+//         $('.carousel').carousel('next');
+//         setTimeout(autoplay, 4500);
+//     }
+// });
+
 $(document).ready(function(){
-    $('.carousel').carousel({
-        fullWidth: true,
-        indicators: true,
-        duration: 500
-    });
-    autoplay()
-    function autoplay(){
-        $('.carousel').carousel('next');
-        setTimeout(autoplay, 4500);
-    }
-});
+    $('.parallax').parallax(
+        
+    );
+  });
